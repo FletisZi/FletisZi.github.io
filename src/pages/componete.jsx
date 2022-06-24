94575f5d0b89420746383a00775202b3563b4177
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2022-06-24 09:50:55 -0300

    Site no ar

---
+++ src/pages/componete.jsx
@@ -0,0 +1,37 @@
+export function section(){
+    return(
+        <section>
+        <form action="">
+            <label>
+                Tipo:
+                <input type="text" name="" id=""/>
+            </label>
+            <label>
+                Nome do titular da UC geradora:
+                <input type="text" name="" id=""/>
+            </label>
+            <label>
+                RG do titular definido da UC
+                <input type="number" name="" id=""/>
+            </label>
+            <label>
+                CPF/CNPJ do tituçar da UC:
+                <input type="number" name="" id=""/>
+            </label>
+            <label>
+                Telefone fixo:
+                <input type="tel" name="" id=""/>
+            </label>
+            <label>
+                Telefone celular:
+                <input type="tel" name="" id=""/>
+            </label>
+            <label>
+                E-mail
+                <input type="email" name="" id=""/>
+            </label>
+        </form>
+        
+    </section>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+import { section } from "./componete";
+
+<section/>
