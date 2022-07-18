e2db4f63b0595ca0b4aa334638775d5e89daac03
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2022-07-18 19:47:32 -0300

    Componente Skill

---
+++ src/styles/components/skills/skills.js
@@ -0,0 +1,7 @@
+let Icone = document.createElement('img');
+let Nome = document.createElement('h1');
+let Nivel = document.createElement('div');
+
+let Obody = document.getElementsByTagName('body');
+
+Obody.appendChild(Nome);
