7616fd2024415961a346c1f1f6dbd15eac798428
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2022-08-06 09:19:04 -0300

    refaturamento layout

--- src/styles/components/skills/skills.js
+++ src/styles/components/skills/skills.js
@@ -1,7 +1,19 @@
-let Icone = document.createElement('img');
-let Nome = document.createElement('h1');
-let Nivel = document.createElement('div');
+    function Start_bar(){
+        let barra = document.createElement('div');
+        barra.className = "barra";
+        let valor = document.createElement('div');
+        valor.className = "valor";
+        let Sobra = document.createElement('div');
+        Sobra.className = "Sobra";
 
-let Obody = document.getElementsByTagName('body');
 
-Obody.appendChild(Nome);
+        barra.appendChild(valor);
+        barra.appendChild(Sobra);
+
+        var classBar = document.querySelector("#classBarra");
+        classBar.appendChild(barra);
+
+    }
+    
+    
+    document.addEventListener("DOMContentLoaded",Start_bar)
