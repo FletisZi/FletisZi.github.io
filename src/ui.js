72aee1935eaa23d3b269a46e6c07883388e0352e
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2022-09-11 13:13:17 -0300

    Adiciona Menu

---
+++ src/ui.js
@@ -0,0 +1,10 @@
+function menu(){
+    
+    let elmento = document.querySelector('#menu2')
+    elmento.style.display = "flex";
+}
+
+function home(){
+    let elmento = document.querySelector('#menu2')
+    elmento.style.display = "none";
+}
